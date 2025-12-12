2cad3c6fcfde1bda0da32b94f26a8ece038437bf
ChonLee <Chon_Lee@iheartmymac.com>
2025-12-11 19:44:24 -0500

    api endpoints missing

---
+++ api/endpoints.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * FPP API Endpoints for Tailscale Plugin
+ * 
+ * This file is required by FPP's API system
+ */
+
+// Function name must match: getEndpoints + plugin name (with dashes removed)
+// For plugin "testing-tailscale-fpp" -> getEndpointstestingtailscalefpp
+function getEndpointstestingtailscalefpp() {
+    $endpoints = array();
+    
+    // Define your API endpoints here
+    // These will be available at /api/plugin/testing-tailscale-fpp/endpoint-name
+    
+    return $endpoints;
+}
